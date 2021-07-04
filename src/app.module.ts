948240ddfecfdc8821bfdbc1521d99dbdd4e783d
Kacper Malkowski <malkowski.kacperum@gmail.com>
2021-07-04 12:20:02 +0200

    add planet model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EpisodesModule } from './episodes/episodes.module';
+import { PlanetsModule } from './planets/planets.module';
 
 @Module({
-  imports: [EpisodesModule],
+  imports: [EpisodesModule, PlanetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
